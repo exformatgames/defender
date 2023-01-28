c28b3c67874cdb01b99b4c7bbc6a65b04fffc433
exformat <exformat1988@gmail.com>
2023-01-28 07:44:40 +0600

    ...

--- src/main/java/com/github/exformatgames/defender/components/ExitComponent.java
+++ src/main/java/com/github/exformatgames/defender/components/ExitComponent.java
@@ -1,7 +1,11 @@
 package com.github.exformatgames.defender.components;
 import com.badlogic.ashley.core.*;
 
-public class ExitComponent implements Component
-{
-	public static ComponentMapper<ExitComponent> mapper = ComponentMapper.getFor(ExitComponent.class);
+public class ExitComponent implements Component {
+
+	private final static ComponentMapper<ExitComponent> mapper = ComponentMapper.getFor(ExitComponent.class);
+
+	public static ExitComponent getComponent(Entity entity) {
+		return mapper.get(entity);
+	}
 }
