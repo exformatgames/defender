b605d0579212c4efe15cf31e38a2089c236990b3
exformat <exformat1988@gmail.com>
2023-01-28 23:28:34 +0600

    reset CullingComponent in RemoveEntitySystem

--- src/main/java/com/github/exformatgames/defender/systems/util_system/RemoveEntitySystem.java
+++ src/main/java/com/github/exformatgames/defender/systems/util_system/RemoveEntitySystem.java
@@ -6,6 +6,7 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.physics.box2d.World;
 import com.github.exformatgames.defender.components.box2d.BodyComponent;
 import com.github.exformatgames.defender.components.light_components.LightComponent;
+import com.github.exformatgames.defender.components.rendering_components.CullingComponent;
 import com.github.exformatgames.defender.components.rendering_components.ParticleEffectComponent;
 import com.github.exformatgames.defender.components.util_components.RemoveEntityComponent;
 import com.github.exformatgames.defender.utils.Particles;
@@ -40,6 +41,11 @@ public class RemoveEntitySystem extends IteratingSystem {
                 Particles.GET(particleEmitterComponent.name).free(particleEmitterComponent.pooledEffect);
 
                 particleEmitterComponent.isDraw = false;
+            }
+
+            CullingComponent cullingComponent = CullingComponent.getComponent(entity);
+            if (cullingComponent != null) {
+                cullingComponent.inViewport = true;
             }
 
             entity.removeAll();
