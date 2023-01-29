df2f95dc71d9df68c9630794c5b53668204c7b4e
exformat <exformat1988@gmail.com>
2023-01-29 16:55:04 +0600

    delegate method setScale(value) in TextRenderComponent

--- src/main/java/com/github/exformatgames/defender/components/rendering_components/ui/TextRenderComponent.java
+++ src/main/java/com/github/exformatgames/defender/components/rendering_components/ui/TextRenderComponent.java
@@ -9,8 +9,24 @@ public class TextRenderComponent implements Component {
     
     public BitmapFont bitmapFont = null;
     public String text = "";
-    
-    private static ComponentMapper<TextRenderComponent> mapper = ComponentMapper.getFor(TextRenderComponent.class);
+
+    public BitmapFont.BitmapFontData bitmapFontData;
+
+    public TextRenderComponent init(BitmapFont bitmapFont, String text) {
+        this.bitmapFont = bitmapFont;
+        this.text = text;
+
+        bitmapFontData = bitmapFont.getData();
+
+        return this;
+    }
+
+    public TextRenderComponent setScale(float scaleXY) {
+        bitmapFontData.setScale(scaleXY);
+        return this;
+    }
+
+    private final static ComponentMapper<TextRenderComponent> mapper = ComponentMapper.getFor(TextRenderComponent.class);
 
     public static TextRenderComponent getComponent(Entity entity) {        
         return mapper.get(entity);
